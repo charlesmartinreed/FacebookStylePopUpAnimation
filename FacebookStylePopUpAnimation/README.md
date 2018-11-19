4fcfddb5fa56c2875a553f973105e870748a8285
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-11-19 17:57:16 -0600

    Complete basic animation of emoji container view.

---
+++ FacebookStylePopUpAnimation/README.md
@@ -0,0 +1,6 @@
+#  Step-by-Step
+1. Register a UILongGestureRecognizer
+2. How do we know where the user is tapping? Figure this out!
+3. Draw the box at the location, on the screen. Use a stack view to contain the emojis.
+4. Animate the emoji views inside of the stackView using hit testing. 
+
--- FacebookStylePopUpAnimation/ViewController.swift
+++ FacebookStylePopUpAnimation/ViewController.swift
@@ -10,11 +10,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- Application Background Properties
+    
+    //not currently using this since I don't have the BG that Brian uses in the demo video...
+//    let bgImageView: UIImageView = {
+//        let imageView = UIImageView(image: "")
+//        return imageView
+//    }()
+    
+    //MARK:- Emoji Icon Container View Properties
+    let iconsContainerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .red
+        
+        containerView.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
+        
+        return containerView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        view.addSubview(bgImageView)
+//        bgImageView.frame = view.frame
+        
+        setupLongPressGesture()
+        
     }
 
+    //MARK:- Gesture methods
+    fileprivate func setupLongPressGesture() {
+        
+        view.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress)))
+    }
+    
+    @objc func handleLongPress(gesture: UILongPressGestureRecognizer) {
+        //print("Long pressed", Date()) //logs a date when we press, and a date when we release.
+        
+        if gesture.state == .began {
+           handleGestureBegan(gesture: gesture)
+        } else if gesture.state == .ended {
+            //remove the previously animated view
+            iconsContainerView.removeFromSuperview()
+        }
+    }
+    
+    fileprivate func handleGestureBegan(gesture: UILongPressGestureRecognizer) {
+        //animate a view INTO application
+        view.addSubview(iconsContainerView)
+        
+        //grab the touch point
+        let pressedLocation = gesture.location(in: self.view)
+        print(pressedLocation)
+        
+        //use a transformation to move the box
+        let centeredX = (view.frame.width - iconsContainerView.frame.width) / 2
+        
+        // gently fade in the iconsContainerView
+        iconsContainerView.alpha = 0
+        
+        //begin the transformation at the pressed location, transform that location to slightly above the touch location by using the iconContainerView's height as an offset
+        self.iconsContainerView.transform = CGAffineTransform(translationX: centeredX, y: pressedLocation.y)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            
+            self.iconsContainerView.alpha = 1
+            self.iconsContainerView.transform = CGAffineTransform(translationX: centeredX, y: pressedLocation.y - self.iconsContainerView.frame.height)
+            
+        })
+    }
 
+    // won't do much until we get the background up
+    override var prefersStatusBarHidden: Bool { return true }
 }
 
